ca307b6358a074140aa3f8d0b2db09427fa1b487
Ivan <ivanidanica@gmail.com>
2021-12-05 11:04:33 +0100

    Update uninstall-driver.sh

--- deploy/uninstall-driver.sh
+++ deploy/uninstall-driver.sh
@@ -21,7 +21,7 @@ if [[ "$#" -gt 0 ]]; then
   ver="$1"
 fi
 
-repo="https://raw.githubusercontent.com/kubernetes-sigs/azurefile-csi-driver/$ver/deploy"
+repo="https://raw.githubusercontent.com/ivankostic85/azurefile-csi-driver/$ver/deploy"
 if [[ "$#" -gt 1 ]]; then
   if [[ "$2" == *"local"* ]]; then
     echo "use local deploy"
